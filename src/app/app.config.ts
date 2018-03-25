616a02f8dcf1a6eb66fadcc08df97a81f9146845
devtactic <devtactic@gmail.com>
2018-03-25 13:49:13 +0100

    Increase vision rotation time to 6 secs

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@ export const AppConfig = {
   // Timeout to initial Idle
   idleTime: 10,
   // Timeout countdown once idleTime has passed
-  idleTimeout: 5,
-  keepAlivePing: 5,
+  idleTimeout: 6,
+  keepAlivePing: 6,
 
 };
